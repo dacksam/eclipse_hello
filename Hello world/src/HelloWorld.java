f4918cb2c8fb2d09bb9ee8b817f893bdf43b306a
sng <sng@daegu.ac.kr>
2019-04-03 10:36:49 +0900

    Create hello world

---
+++ Hello world/src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello his World");
+		System.out.println("Hello your World");
+		System.out.println("Hello her World");
+	}
+}
